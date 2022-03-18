112f69fe33199063862e7593b4966270f71ff6b1
Brian <briannokanga@gmail.com>
2022-03-18 15:50:42 +0300

    Added the ExpensesRepository

--- ExpenseManagerLite/IExpenseService.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ExpenseManagerLite
-{
-    public interface IExpenseService
-    {
-    }
-}
---
+++ ExpenseManagerLite/Interfaces/IExpenseService.cs
@@ -0,0 +1,17 @@
+﻿using ExpenseManagerLite.Models;
+using System.Collections.Generic;
+
+namespace ExpenseManagerLite.Interfaces
+{
+    public interface IExpenseService
+    {
+        IEnumerable<ExpenseReport> GetAllExpenses();
+        IEnumerable<ExpenseReport> GetSearchResult(string searchString);
+        void AddExpense(ExpenseReport expense);
+        int UpdateExpense(ExpenseReport expense);
+        ExpenseReport GetExpenseData(int id);
+        void DeleteExpense(int id);
+        Dictionary<string, decimal> CalculateMonthlyExpense();
+        Dictionary<string, decimal> CalculateWeeklyExpense();
+    }
+}

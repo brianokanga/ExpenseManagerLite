b24fc09f914673882ba5b47563962dc18a620dd7
Brian <briannokanga@gmail.com>
2022-03-18 16:18:18 +0300

    Added Shared layout coniguration

---
+++ ExpenseManagerLite/Controllers/ExpenseController.cs
@@ -0,0 +1,80 @@
+﻿using ExpenseManagerLite.Interfaces;
+using ExpenseManagerLite.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExpenseManagerLite.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        private readonly IExpenseService expenseService;
+
+        public ExpenseController(IExpenseService _expenseService)
+        {
+            expenseService = _expenseService;
+        }
+        public IActionResult Index(string searchString)
+        {
+            List<ExpenseReport> lstEmployee = new List<ExpenseReport>();
+            lstEmployee = expenseService.GetAllExpenses().ToList();
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                lstEmployee = expenseService.GetSearchResult(searchString).ToList();
+            }
+            return View(lstEmployee);
+        }
+
+        public ActionResult AddEditExpenses(int itemId)
+        {
+            ExpenseReport model = new ExpenseReport();
+            if (itemId > 0)
+            {
+                model = expenseService.GetExpenseData(itemId);
+            }
+            return PartialView("_expenseForm", model);
+        }
+
+        [HttpPost]
+        public ActionResult Create(ExpenseReport newExpense)
+        {
+            if (ModelState.IsValid)
+            {
+                if (newExpense.ItemId > 0)
+                {
+                    expenseService.UpdateExpense(newExpense);
+                }
+                else
+                {
+                    expenseService.AddExpense(newExpense);
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            expenseService.DeleteExpense(id);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult ExpenseSummary()
+        {
+            return PartialView("_expenseReport");
+        }
+
+        public JsonResult GetMonthlyExpense()
+        {
+            Dictionary<string, decimal> monthlyExpense = expenseService.CalculateMonthlyExpense();
+            return new JsonResult(monthlyExpense);
+        }
+
+        public JsonResult GetWeeklyExpense()
+        {
+            Dictionary<string, decimal> weeklyExpense = expenseService.CalculateWeeklyExpense();
+            return new JsonResult(weeklyExpense);
+        }
+    }
+}
